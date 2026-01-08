731e793c7bea6f3ee0464297fcf685e3808137a4
Mohamed Akbarally <mohamed.akbarally@gmail.com>
2026-01-09 10:08:54 +1100

    use FINISH_TOOL_NAME instead of 'finish'

--- src/stirrup/tools/finish.py
+++ src/stirrup/tools/finish.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from pydantic import BaseModel, Field
 
+from stirrup.constants import FINISH_TOOL_NAME
 from stirrup.core.models import Tool, ToolResult, ToolUseCountMetadata
 
 
@@ -15,7 +16,7 @@ class FinishParams(BaseModel):
 
 
 SIMPLE_FINISH_TOOL: Tool[FinishParams, ToolUseCountMetadata] = Tool[FinishParams, ToolUseCountMetadata](
-    name="finish",
+    name=FINISH_TOOL_NAME,
     description="Signal task completion with a reason. Use when the task is finished or cannot proceed further. Note that you will need a separate turn to finish.",
     parameters=FinishParams,
     executor=lambda params: ToolResult(content=params.reason, metadata=ToolUseCountMetadata(), success=True),
